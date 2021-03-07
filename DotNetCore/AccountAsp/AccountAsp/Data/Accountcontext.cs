579945c8599110656c84a6490ec3f81df4ac811f
JayashriRavishankar <jayasriravi98@gmail.com>
2021-03-07 20:48:25 +0530

    Handson

---
+++ DotNetCore/AccountAsp/AccountAsp/Data/Accountcontext.cs
@@ -0,0 +1,22 @@
+﻿using AccountAsp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace AccountAsp.Data
+{
+    public class Accountcontext : DbContext
+    {
+        public Accountcontext(DbContextOptions<Accountcontext> options) : base(options)
+           
+        {
+        }
+
+        public DbSet<AccountAsp.Models.Account> Account { get; set; }
+    }
+}
+
