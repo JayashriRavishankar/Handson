f6e6a389071cb0341d01d96cbfd740ed32c29738
JayashriRavishankar <jayasriravi98@gmail.com>
2021-03-07 15:11:47 +0530

    Handson

---
+++ DotNetCore/ASP_Partial_Emp/ASP_Partial_Emp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_Partial_Emp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
